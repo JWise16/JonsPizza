e8e3388af7d960cb0b783594ebd753368fe44339
JWise16 <68126980+JWise16@users.noreply.github.com>
2025-01-06 17:16:09 +0000

    started working on product details

--- FoodOrdering/src/app/(tabs)/menu/[id].tsx
+++ FoodOrdering/src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,50 @@
 import { useLocalSearchParams, Stack } from "expo-router";
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
+import React, {useState} from 'react';
 
+import products from '@/assets/data/products';
+import { ScrollViewStyleReset } from "expo-router/html";
+
+const sizes =['S', 'M', 'L', 'XL'];
 
 const ProductDetailsScreen = () => {
     const {id} = useLocalSearchParams();
+
+    const product = products.find((p) => p.id.toString() == id);
+
+    if(!product){
+        return <Text>Product not found</Text>
+    }
+
     return(
-        <View>
+        <View style ={styles.container}>
             <Stack.Screen options ={{title: 'Details'}}/>
-            <Text>Product Details for ID: {id}</Text>
+            <Image source={{uri: product.image || '@/assets/images/icon.png'}} style={styles.image}/>
+            <Text>Select size</Text>
+            
+            <Text style={styles.price}>Price: ${product.price}</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+container:{
+    backgroundColor: 'white',
+    flex:1,
+    padding:10
+
+},
+image: {
+    width: '100%',
+    aspectRatio:1,
+
+},
+price: {
+    fontSize:18,
+    fontWeight:'bold',
+
+}
+
+})
 
 export default ProductDetailsScreen;
