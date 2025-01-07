e1c9baee7a11942f8f04ecd877df9cf9a43915ef
JWise16 <68126980+JWise16@users.noreply.github.com>
2025-01-07 18:18:10 +0000

    almost done with UI for products page!

--- FoodOrdering/src/app/(tabs)/menu/[id].tsx
+++ FoodOrdering/src/app/(tabs)/menu/[id].tsx
@@ -3,12 +3,15 @@ import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
 import React, {useState} from 'react';
 
 import products from '@/assets/data/products';
-import { ScrollViewStyleReset } from "expo-router/html";
+
+
 
 const sizes =['S', 'M', 'L', 'XL'];
 
 const ProductDetailsScreen = () => {
     const {id} = useLocalSearchParams();
+
+    const [selectedSize, setSelectedSize] = useState('M');
 
     const product = products.find((p) => p.id.toString() == id);
 
@@ -21,7 +24,15 @@ const ProductDetailsScreen = () => {
             <Stack.Screen options ={{title: 'Details'}}/>
             <Image source={{uri: product.image || '@/assets/images/icon.png'}} style={styles.image}/>
             <Text>Select size</Text>
-            
+
+            <View style={styles.sizes}>
+                {sizes.map((size) =>(
+                    <Pressable onPress = {() => {setSelectedSize(size)}} style={[styles.size, {backgroundColor: selectedSize == size ? 'gainsboro' : 'white'}]} key={size}>
+                        <Text style={[styles.sizeText, {color: selectedSize == size? 'black': 'grey'}]}>{size}</Text>
+                    </Pressable>
+                ))} 
+            </View>
+
             <Text style={styles.price}>Price: ${product.price}</Text>
         </View>
     );
@@ -42,6 +53,24 @@ image: {
 price: {
     fontSize:18,
     fontWeight:'bold',
+
+},
+sizes:{
+    flexDirection:'row',
+    justifyContent:'space-around',
+    marginVertical:10
+},
+size:{
+    backgroundColor:'gainsboro',
+    width:50,
+    aspectRatio:1,
+    borderRadius:25,
+    alignItems:'center',
+    justifyContent:'center'
+},
+sizeText:{
+    fontSize:20,
+    fontWeight:'500',
 
 }
 
